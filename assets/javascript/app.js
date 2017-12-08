e5e541a874fd949ad09c7c04fe662700eadd7816
juan rivas <jjrivasjr1@gmail.com>
2017-12-08 16:25:21 -0600

    most recent edit

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,9 @@
 window.onload = function () {
-	$("#start").click(stopWatch.start);
+	$("#start").click(function() {
+		stopWatch.start();
+		(".hidden").toggle();
+		// $(".hidden").css("display", "inline");
+	});
 	$("#stop").click(stopWatch.stop);
 	$("#reset").click(stopWatch.reset);
 };
@@ -14,7 +18,19 @@ var stopWatch = {
 	},
 	start: function () {
 		//TODO: Use setInterval to start the count here.
+		// $(".hidden").css("display","block");
+		// $('.hidden').toggle();
+		$('.hidden').addClass('show').removeClass('hidden');
 		counter = setInterval(stopWatch.count, 1000);
+
+		// $(".hidden").click(show());
+		// // var hiddenInputs = document.getElementsById("hidden");
+		// // this.style.display = "block";
+		// function show() {
+		// 	if($(".hidden").css('display') == "none"){
+		// 		$(".hidden").css('display', 'none');
+		// 	}
+		// }
 
 	},
 	stop: function () {
@@ -70,4 +86,34 @@ function submitAnswers () {
 	var q4 = document.forms["quizForm"]["q4"].value;
 	var q5 = document.forms["quizForm"]["q5"].value;
 
-}
+	// validation
+
+	for(i = 1; i <= total; i++){
+		if(eval("q"+ i)  == null || eval("q"+ i) == ''){
+			$("#missed").html("<h3>YOU MISSED QUERSTION <span>"+ i+"</span></h3>");
+
+			return false;
+		}
+
+	}
+
+
+	//set correct answers
+	var answers = ['d', 'c', 'c', 'd', 'd'];
+
+	//check answers
+		for(i = 1; i <= total; i++){
+			if(eval("q"+ i)== answers[i-1]) {
+				score++;
+			}	
+		}
+		// Display Results
+		$("#results").html("<h3>YOU SCORED <span>"+score+"</span> OUT OF <span>"+total+"</span></h3>");
+
+		return false;
+
+	}
+
+
+
+
